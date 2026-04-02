f67f9dd15c38a376060cefb8632044005cde16bb
kshitizmishra <kshitizmishra3@gmail.com>
2026-04-02 23:05:01 +0530

    Leat code problem 73.setmatrix zero

--- src/main/java/org/LeatCode/prblms/Array/SetMatrixZeroes.java
+++ src/main/java/org/LeatCode/prblms/Array/SetMatrixZeroes.java
@@ -12,13 +12,11 @@ public class SetMatrixZeroes {
         ArrayList<Integer> ycol = new ArrayList<>();
         for(int i=0 ;i<matrix.length;i++){
             for (int j=0; j< matrix[i].length ; j++){
-                System.out.print(matrix[i][j]+" ");
                 if( matrix[i][j]==0){
                     xcol.add(i);
                     ycol.add(j);
                 }
             }
-            System.out.println();
         }
         for(int i : xcol){
             for(int j=0;j<matrix[i].length;j++){
@@ -30,16 +28,81 @@ public class SetMatrixZeroes {
                 matrix[i][j]=0;
             }
         }
+
+    }
+
+    public static void optSetZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+
+        // Check if first row has zero
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        // Check if first column has zero
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+
+        // Use first row and column as markers
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Set zeroes based on markers
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Zero out first row if needed
+        if (firstRowZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Zero out first column if needed
+        if (firstColZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1,1,1},{1,0,1},{1,1,1}};
         for(int i=0 ;i<matrix.length;i++){
             for (int j=0; j< matrix[i].length ; j++){
                 System.out.print(matrix[i][j]+" ");
             }
             System.out.println();
         }
-    }
-
-    public static void main(String[] args) {
-        int[][] matrix = {{0,1,0,1},{1,1,1,1},{1,1,1,1}};
-        setZeroes(matrix);
+        //to use better approach unclomment this function
+        //setZeroes(matrix);
+        optSetZeroes(matrix);
+        for(int i=0 ;i<matrix.length;i++){
+            for (int j=0; j< matrix[i].length ; j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
