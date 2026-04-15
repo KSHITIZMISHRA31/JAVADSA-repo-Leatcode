6d497a6ac44d59b421cab51f6941592a65050d1b
kshitizmishra <kshitizmishra3@gmail.com>
2026-04-15 23:54:12 +0530

    2515. Shortest Distance to Target String in a Circular Array

---
+++ src/main/java/org/LeatCode/prblms/String/ShrtstDistnctoTrgtStrngCrclrAry.java
@@ -0,0 +1,37 @@
+package org.LeatCode.prblms.String;
+
+public class ShrtstDistnctoTrgtStrngCrclrAry {
+
+    public static int closestTarget(String[] words, String target, int startIndex) {
+
+        int nrstRght = Integer.MAX_VALUE;
+        int nrstLft = Integer.MAX_VALUE;
+        int count = 0;
+        for(int i = 0 ;i<words.length;i++){
+            if(words[(i+startIndex)%words.length].equals(target)){
+                nrstRght = Math.min(count,nrstRght);
+            }
+            count++;
+        }
+        if(nrstRght == Integer.MAX_VALUE){
+            return -1;
+        }
+        count = 0;
+        for(int i = words.length ;i>-1;i--){
+            if(words[(i+startIndex)%words.length].equals(target)){
+                nrstLft = Math.min(count,nrstLft);
+            }
+            count++;
+        }
+
+        return Math.min(nrstLft,nrstRght);
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"hello","i","am","leetcode","hello"};
+        String target = "hello";
+        int startIndex = 1;
+
+        System.out.println(closestTarget(words,target,startIndex));
+    }
+}
