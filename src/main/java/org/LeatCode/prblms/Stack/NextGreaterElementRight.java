9547fc4e568e390e3e843d741c7db2c93813eca9
kshitizmishra <kshitizmishra3@gmail.com>
2026-04-29 22:44:51 +0530

    NextGreaterElementRight

---
+++ src/main/java/org/LeatCode/prblms/Stack/NextGreaterElementRight.java
@@ -0,0 +1,37 @@
+package org.LeatCode.prblms.Stack;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class NextGreaterElementRight {
+
+    /// =======================
+    /// Optimized Approach
+    /// ======================
+    public static int[] nextGreaterElementRight(int[] arr){
+        int[] rslt = new int[arr.length];
+        System.out.println(Arrays.toString(arr));
+        Stack<Integer> st = new Stack<>();
+        for(int i =arr.length-1 ;i>=0;i--){
+            while(!st.isEmpty() && st.peek()<=arr[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                rslt[i] = -1;
+            }
+            else{
+                rslt[i] = st.peek();
+            }
+            st.push(arr[i]);
+        }
+        System.out.println(st);
+
+        return rslt;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,10,12,1,11};
+        System.out.println(Arrays.toString(nextGreaterElementRight(nums)));
+
+    }
+}
