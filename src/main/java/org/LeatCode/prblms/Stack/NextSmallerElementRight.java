8d3d7d3d85c3a6e36ef345fb48e1006a1b2f5f66
kshitizmishra <kshitizmishra3@gmail.com>
2026-04-29 22:59:18 +0530

    NextSmallerElementRight

---
+++ src/main/java/org/LeatCode/prblms/Stack/NextSmallerElementRight.java
@@ -0,0 +1,35 @@
+package org.LeatCode.prblms.Stack;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class NextSmallerElementRight {
+    public static int[] nextSmallerElementRight(int[] arr){
+        int[] rslt = new int[arr.length];
+        System.out.println(Arrays.toString(arr));
+        Stack<Integer> st = new Stack<>();
+        for(int i =0;i< arr.length;i++){
+            while(!st.isEmpty() && st.peek()>=arr[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                rslt[i] = -1;
+            }
+            else{
+                rslt[i] = st.peek();
+            }
+            st.push(arr[i]);
+        }
+        System.out.println(st);
+
+        return rslt;
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] nums = {2,10,12,1,11};
+        System.out.println(Arrays.toString(nextSmallerElementRight(nums)));
+
+    }
+}
