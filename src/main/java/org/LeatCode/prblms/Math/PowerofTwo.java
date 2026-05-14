a66cc20f90505583f1a2d02e98f412e6160ace50
kshitizmishra <kshitizmishra3@gmail.com>
2026-05-14 21:59:00 +0530

    231. Power of Two

---
+++ src/main/java/org/LeatCode/prblms/Math/PowerofTwo.java
@@ -0,0 +1,22 @@
+package org.LeatCode.prblms.Math;
+
+public class PowerofTwo {
+
+    public static boolean isPowerOfTwo(int n) {
+        if (n == 0){
+            return false;
+        }
+        while(n!=1){
+            if(n%2 != 0){
+                return false;
+            }
+            n = (int)n/2;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int n = 16;
+        System.out.println(isPowerOfTwo(n));
+    }
+}
