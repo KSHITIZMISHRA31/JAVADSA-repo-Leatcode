41a3ba693b0db3e69ede667d52039bffb631acf8
kshitizmishra <kshitizmishra3@gmail.com>
2026-05-20 00:34:46 +0530

    69. Sqrt(x)

---
+++ src/main/java/org/LeatCode/prblms/Math/Sqrtx.java
@@ -0,0 +1,15 @@
+package org.LeatCode.prblms.Math;
+
+public class Sqrtx {
+    public static int mySqrt(int x) {
+
+        System.out.println((int)Math.sqrt(x));
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int x = 8;
+        System.out.println(mySqrt(x));
+    }
+}
