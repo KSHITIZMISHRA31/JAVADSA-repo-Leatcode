9f03c4c975027155651de684bec7590f9c2afe44
kshitizmishra <kshitizmishra3@gmail.com>
2026-06-07 12:01:11 +0530

    678. Valid Parenthesis String

---
+++ src/main/java/org/LeatCode/prblms/Greedy/ValidParenthesisString.java
@@ -0,0 +1,48 @@
+package org.LeatCode.prblms.Greedy;
+
+import java.util.Stack;
+
+public class ValidParenthesisString {
+
+    public static boolean checkValidString(String s) {
+        Stack<Integer> open = new Stack<>();
+        Stack<Integer> star = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                open.push(i);
+            }
+            else if (ch == '*') {
+                star.push(i);
+            }
+            else { // ')'
+                if (!open.isEmpty()) {
+                    open.pop();
+                }
+                else if (!star.isEmpty()) {
+                    star.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        while (!open.isEmpty() && !star.isEmpty()) {
+            if (open.peek() > star.peek()) {
+                return false;
+            }
+            open.pop();
+            star.pop();
+        }
+
+        return open.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s = "((*))*))";
+        System.out.println(checkValidString(s));
+    }
+}
